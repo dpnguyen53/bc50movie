8b19e8fe0a125c36e45282e0ce80b2c5f3ced5a9
dpnguyen53 <dpnguyen53@gmail.com>
2023-07-10 21:27:33 +0700

    Buoi 8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminTemplate from "./pages/AdminTemplate";
+import HomeTemplate from "./pages/HomeTemplate";
+import HomePage from "./pages/HomeTemplate/HomePage";
+import AboutPage from "./pages/HomeTemplate/AboutPage";
+import ListMoviePage from "./pages/HomeTemplate/ListMoviePage";
+import Dashboard from "./pages/AdminTemplate/Dashboard";
+import AddUser from "./pages/AdminTemplate/AddUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* HomeTemplate - localhost:3000 */}
+        <Route path="" element={<HomeTemplate />}>
+          <Route path="" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="list-movie" element={<ListMoviePage />} />
+        </Route>
+
+        {/* Admin Template */}
+        <Route path="admin" element={<AdminTemplate />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="add-user" element={<AddUser />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  //child reducer
+});
+
+export default rootReducer;
