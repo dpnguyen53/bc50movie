f5852b23915fe8c92f4603f28717e2e3585bc667
dpnguyen53 <dpnguyen53@gmail.com>
2023-07-24 21:30:29 +0700

    buoi 12

--- src/pages/AdminTemplate/AuthPage/duck/actions.js
+++ src/pages/AdminTemplate/AuthPage/duck/actions.js
@@ -1,6 +1,44 @@
 import { AUTH_REQUEST, AUTH_SUCCESS, AUTH_FAIL } from "./constants";
+import api from "./../../../../utils/apiUtil";
 
-export const actAuth = () => {};
+export const actAuth = (user, navigate) => {
+  return (dispatch) => {
+    dispatch(actAuthRequest());
+
+    api
+      .post("QuanLyNguoiDung/DangNhap", user)
+      .then((result) => {
+        if (result.data.statusCode === 200) {
+          console.log(result.data);
+          const user = result.data.content;
+
+          if (!(user.maLoaiNguoiDung === "QuanTri")) {
+            //show error
+            const error = {
+              response: {
+                data: {
+                  content: "Bạn không có quyền truy cập!",
+                },
+              },
+            };
+            return Promise.reject(error);
+          }
+
+          //QuanTri => lưu thông tin user
+          dispatch(actAuthSuccess(user));
+
+          //QuanTri => lưu trang thái login
+          localStorage.setItem("UserAdmin", JSON.stringify(user));
+
+          //QuanTri => redirect admin/dashboard
+          navigate("/admin/dashboard", { replace: true });
+        }
+      })
+      .catch((error) => {
+        dispatch(actAuthFail(error));
+      });
+  };
+};
 
 const actAuthRequest = () => {
   return {
--- src/pages/AdminTemplate/AuthPage/index.js
+++ src/pages/AdminTemplate/AuthPage/index.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
+import { actAuth } from "./duck/actions";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, Navigate } from "react-router-dom";
 
 export default function AuthPage() {
+  const error = useSelector((state) => state.authReducer.error);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const [state, setState] = useState({
     taiKhoan: "",
     matKhau: "",
@@ -16,15 +23,28 @@ export default function AuthPage() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log(state);
     //gọi tới action call api
+    dispatch(actAuth(state, navigate));
   };
+
+  const renderError = () => {
+    return (
+      error && (
+        <div className="alert alert-danger">{error?.response.data.content}</div>
+      )
+    );
+  };
+
+  if (localStorage.getItem("UserAdmin")) {
+    return <Navigate replace to="/admin/dashboard" />;
+  }
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 mx-auto">
           <h3>AuthPage</h3>
+          {renderError()}
           <form onSubmit={handleLogin}>
             <div className="form-group">
               <label>Tài khoản</label>
